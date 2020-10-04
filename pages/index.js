a3561d5e46b36e652fb4d87e92db124529d60f9c
Ramajedd <40004459+ramajedd@users.noreply.github.com>
2020-10-04 05:31:02 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Home = () => {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hi {data.users[0].name}</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
