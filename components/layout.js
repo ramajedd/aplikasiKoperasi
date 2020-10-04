7c4af95cb4b59460b8b798c36a0acfbeff195394
Ramajedd <40004459+ramajedd@users.noreply.github.com>
2020-10-04 05:22:28 -0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ const name = 'Your Name'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
-  console.log(children);
+  //console.log(children);
   return (
     <div className={styles.container}>
       <Head>
