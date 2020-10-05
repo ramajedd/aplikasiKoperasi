74bb99fae9f3a3b9e223f88d6c6bdbf59e631c89
Ramajedd <40004459+ramajedd@users.noreply.github.com>
2020-10-05 02:59:23 -0700

    Update first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Layout from '../../components/layout'
-import Alert from '../../components/Alert'
+import Alert from '../../components/alert'
 
 export default function FirstPost() {
   return (
