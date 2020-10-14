a78589f0bb7b863dfc9047bebd9c7ca0c17d5015
Ramajedd <40004459+ramajedd@users.noreply.github.com>
2020-10-14 07:54:30 -0700

    menambahkan purge css
    percobaan mengatasi masalah yang terjadi sebelumnya ; css datepicker tidak terender di vercel

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,26 @@ import { withApollo } from '../libs/apollo';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_USERS } from '../gql/users';
 
+//import "../css/datePicker.css";
+import DatePicker from "react-modern-calendar-datepicker";
+import React, { useState } from "react";
+
+const App = () => {
+  const [selectedDay, setSelectedDay] = useState(null);
+
+  // render regular HTML input element
+ 
+
+  return (
+    <DatePicker
+      value={selectedDay}
+      onChange={setSelectedDay}
+      //renderInput={renderCustomInput} // render a custom input
+      shouldHighlightWeekends
+    />
+  );
+};
+
 const Home = () => {
   const { loading, error, data } = useQuery(GET_USERS);
   if (error) return <h1>Error</h1>;
@@ -16,6 +36,7 @@ const Home = () => {
         <title>{siteTitle}</title>
       </Head>
       <section >
+        <App/>
         <LayOutKonten type='home'>
           hi {data.users[0].name}
           ini adalah children dari file index
--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,24 @@
 const purgecss = [
   "@fullhuman/postcss-purgecss",
   {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    content: [
+      './pages/**/*.{js,jsx,ts,tsx}',
+      './components/**/*.{js,jsx,ts,tsx}',
+    ],
+    defaultExtractor: (content) => {
+      // Capture as liberally as possible, including things like `h-(screen-1.5)`
+      const broadMatches = content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || [];
+      // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+      const innerMatches = content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || [];
+      return broadMatches.concat(innerMatches);
+    },
   }
 ];
 module.exports = {
   plugins: [
     "postcss-import",
     "tailwindcss",
+    "postcss-preset-env",
     "autoprefixer",
     ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
