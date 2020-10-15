eeb5bc45402884a9c8ac356c603163e7aa7fcc09
Ramajedd <40004459+ramajedd@users.noreply.github.com>
2020-10-15 00:34:30 -0700

    Update boxBuyer.js
    menambahkan usestate

--- components/boxBuyer.js
+++ components/boxBuyer.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import DatePicker from "./datePickerModal"
 
 
 export default function BoxBuyer(props) {
-        
+    const [isMounted, setIsMounted] = useState()
+    useEffect(() => {
+        setIsMounted(<DatePicker/>)
+      }, [])
     return (
         <div className=" pt-1 pb-2 px-4 ">
             <div className="flex bg-gray-300 rounded">
                 <div className="bg-gray-300 px-4 py-4 rounded">
                     <div className="font-sans sm:font-serif md:font-mono lg:font-sans xl:font-serif">
-                        <DatePicker/>
+                        {isMounted}
                     </div>
                     <div className="font-sans sm:font-serif md:font-mono lg:font-sans xl:font-serif">
                         Pengguna (ubah)
